2cafb5315c01eeda7e52d9bf4b252839c903a9eb
Kavitha <100946039+Kavitha-Raya02@users.noreply.github.com>
2024-03-27 16:48:48 +0530

    Add files via upload

--- DBPropertyUtil.java
+++ DBPropertyUtil.java
@@ -1,33 +1,11 @@
 package com.util;
 
-import java.io.InputStream;
-import java.io.IOException;
-import java.util.Properties;
-
 public class DBPropertyUtil {
-    private static final String path = "db.properties";
-    public static String getPropertyString() 
-    {
-    	 String connectionString = null;
-        Properties properties = new Properties();
-        try (InputStream inputStream = DBPropertyUtil.class.getResourceAsStream(path)) {
-           
-                properties.load(inputStream);
-                connectionString = "jdbc:mysql://" +
-                        properties.getProperty("hostname") +
-                        ":" + properties.getProperty("port") +
-                        "/" + properties.getProperty("dbname") +
-                        "?user=" + properties.getProperty("username") +
-                        "&password=" + properties.getProperty("password");
-                return connectionString;
-            } 
-         catch (IOException e) {
-            e.printStackTrace();
-        
-        }
-		return connectionString;
+	
+	public static String getConnectionString(String fileName) {
 		
-    }
-    
-   
+		String conString = "jdbc:mysql://127.0.0.1:3306/"+fileName+"\", \"root\", \"phoenix\"";
+		return conString;
+	}
+
 }
